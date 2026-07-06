183d56e21283709940d47067c67b2e20f08fdfa4
Dooms10 <bhumikadhingra2003@gmail.com>
2026-07-07 00:48:19 +0530

    Restore original banner image positioning while retaining single line text scaling

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -725,7 +725,7 @@ export default function HomeDashboard() {
               style={{ 
                 flex: 1, 
                 paddingLeft: windowWidth < 380 ? 16 : 24, 
-                paddingRight: windowWidth < 380 ? 115 : (Platform.OS === 'web' ? 260 : 180),
+                paddingRight: windowWidth < 380 ? 145 : (Platform.OS === 'web' ? 260 : 180),
                 justifyContent: 'center'
               }}
             >
@@ -762,8 +762,8 @@ export default function HomeDashboard() {
               source={require('../../../assets/images/rings_only_banner.png')}
               style={{ 
                 position: 'absolute',
-                right: windowWidth < 380 ? 45 : (Platform.OS === 'web' ? 100 : 70),
-                width: windowWidth < 380 ? 70 : (Platform.OS === 'web' ? 125 : 107),
+                right: Platform.OS === 'web' ? 100 : 70,
+                width: Platform.OS === 'web' ? 125 : 107,
                 height: '100%'
               }}
               contentFit="contain"
@@ -775,7 +775,7 @@ export default function HomeDashboard() {
               style={{ 
                 position: 'absolute',
                 right: 0,
-                width: windowWidth < 380 ? 60 : (Platform.OS === 'web' ? 105 : 90),
+                width: Platform.OS === 'web' ? 105 : 90,
                 height: '100%'
               }}
               contentFit="contain"
